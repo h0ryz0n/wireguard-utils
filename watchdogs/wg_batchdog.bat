38b86e9a7aee9f4fb096c2f9eeb21223bb5a7c15
gck0 <f.alfieri@outlook.it>
2021-03-17 01:53:09 +0100

    Create wg_batchdog.bat

---
+++ watchdogs/wg_batchdog.bat
@@ -0,0 +1,30 @@
+@echo off
+::BATCHDOG: A WINDOWS BATCH-WATCHDOG TO FOR A WIREGUARD BOUNCE SERVER/PEER WITH DDNS
+::restarts the windows service to update the dns entry, when needed
+setlocal
+
+:start
+for /f "tokens=2" %%a IN ( 
+'wg show wg0 latest-handshakes' 
+) do ( 
+set wglasthandshake=%%a
+) 
+
+:loop
+timeout /T 60 /nobreak > nul
+for /f "tokens=2" %%a IN ( 
+'wg show wg0 latest-handshakes' 
+) do ( 
+if %wglasthandshake% == %%a (goto restartservice)
+)
+goto loop
+
+:restartservice
+sc stop "WireguardTunnel$wg0" > nul
+timeout /T 1 /nobreak > nul
+sc start "WireguardTunnel$wg0" > nul
+::wait for first handshake and restart
+timeout /T 10 /nobreak
+
+endlocal
+goto init
