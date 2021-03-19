a5f720e1f1f942ecb0a5cb755b78ed2320673088
gck0 <f.alfieri@outlook.it>
2021-03-19 16:33:45 +0100

    Create wg_batchdog.ps1

---
+++ watchdog/wg_batchdog.ps1
@@ -0,0 +1,6 @@
+::@echo off
+::BATCHDOG: A WINDOWS BATCH-WATCHDOG TO FOR A WIREGUARD BOUNCE SERVER/PEER WITH DDNS (POWERSHELL VERSION)
+::restarts the windows service to update the dns entry, when needed
+::add to task scheduler with nt authority/system account
+::
+::todo
