18ceee01d1ccc0f4fdfe4dd47b60d74d93d0ad9a
gck0 <f.alfieri@outlook.it>
2021-03-27 11:22:24 +0100

    Update wg_batchdog.bat

--- watchdog/wg_batchdog.bat
+++ watchdog/wg_batchdog.bat
@@ -1,10 +1,11 @@
 ::@echo off
-::BATCHDOG: A WINDOWS BATCH-WATCHDOG FOR A WIREGUARD BOUNCE SERVER/PEER WITH DDNS
+::BATCHDOG: A WINDOWS (RAW) BATCH-WATCHDOG FOR A WIREGUARD BOUNCE SERVER/PEER WITH DDNS
 ::restarts the windows service to update the dns entry, when needed
 ::add to task scheduler with nt authority/system account (in case of error restart 999 times every 5 minutes)
 setlocal
 
 :init
+net start "WireguardTunnel$wg0"
 for /f "tokens=2" %%a IN ( 
 'wg show wg0 transfer' 
 ) do (
